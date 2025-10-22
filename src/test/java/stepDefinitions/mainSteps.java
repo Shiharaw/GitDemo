ca27aff8617c3e79595a5c16f2964529cce884e9
Shiharaw <shiharasss@gmail.com>
2025-10-22 12:21:07 +0530

    ShiharUS 1st commit

--- src/test/java/stepDefinitions/mainSteps.java
+++ src/test/java/stepDefinitions/mainSteps.java
@@ -33,6 +33,11 @@ public class mainSteps {
 		System.out.println(data.get(1));
 		System.out.println(data.get(2));
 		System.out.println(data.get(3));
+		//Changes to file for JIT demo purpose
+		System.out.println(data.get(0));
+		System.out.println(data.get(1));
+		System.out.println(data.get(2));
+		System.out.println(data.get(3));
 	}
 	
 
